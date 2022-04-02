2313ab5429c23d5aa2351bd55d0b1cffc273ba89
Marko Kosunen <marko.kosunen@aalto.fi>
2022-04-02 08:33:27 +0300

    Update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,23 @@
 #/usr/bin/env bash
 
-#rm -rf /thesdk_template
+#if [ -d /thesdk_template ]; then
+#    rm -rf /thesdk_template
+#fi
 #git clone https://github.com/TheSystemDevelopmentKit/thesdk_template.git
-
+#
 #cd /thesdk_template
 #git checkout v1.8_RC
-
+#
 #./configure
-
+#
 #sed -i 's#\(url = \)\(git@\)\(.*\)\(:\)\(.*$\)#\1https://\3/\5#g' .gitmodules && git submodule sync && git submodule update --init
 #./pip3userinstall.sh
-
+#
 #cd doc/docstrings
-
+#
 #make html
 #cat build/html/index.html
-OUT="Hello world"
+OUT="Success"
 echo $OUT
 echo "::set-output name=outstring::$OUT"
 exit 0
