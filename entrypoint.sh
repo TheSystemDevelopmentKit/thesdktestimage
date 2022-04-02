6a52476cbde89082f924de6a2feaf216da3ed036
Marko Kosunen <marko.kosunen@aalto.fi>
2022-04-02 16:33:47 +0300

    Update

--- entrypoint.sh
+++ entrypoint.sh
@@ -35,7 +35,7 @@ git add -A
 git config --global user.name "$GITHUB_ACTOR"
 git config --global user.email "$MAIL"
 git commit -m"Update docs" && 
-git remote set-url origin https://${GITHUB_ACTOR}:${TOKEN}@github.com/TheSystemDevelopmentKit/docs.git
+git remote set-url origin https://${TOKEN}@github.com/TheSystemDevelopmentKit/docs.git
 
 git push 
 
